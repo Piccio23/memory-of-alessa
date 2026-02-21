0bcccbf5bd130472a4ff120ff4f4c954eb5360c6
moth dreams <mothdreams666@gmail.com>
2026-02-21 00:20:47 -0800

    add `libdma.h`

--- include/eeregs.h
+++ include/eeregs.h
@@ -10,4 +10,28 @@
 #define D0_ASR0         ((volatile u_int *)(0x10008040))
 #define D0_ASR1         ((volatile u_int *)(0x10008050))
 
+/* VU0 */
+#define VU0_MICRO       ((volatile u_long *)(0x11000000))
+#define VU0_MEM         ((volatile u_long128 *)(0x11004000))
+
+/* VU1 */
+#define VU1_MICRO       ((volatile u_long *)(0x11008000))
+#define VU1_MEM         ((volatile u_long128 *)(0x1100c000))
+
+//
+// Bitfield Structure
+//
+typedef struct {
+	unsigned DIR: 1;	// Direction
+	unsigned p0 : 1;
+	unsigned MOD: 2;	// Mode
+	unsigned ASP: 2;	// Address stack pointer
+	unsigned TTE: 1;	// Tag trasfer enable
+	unsigned TIE: 1;	// Tag interrupt enable
+	unsigned STR: 1;	// start
+	unsigned p1 : 7;
+	unsigned TAG:16;	// DMAtag
+} tD_CHCR;
+
 #endif EE_REGS_H
+
