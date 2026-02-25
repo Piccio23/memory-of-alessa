58239ba3af66fbe44cb254eed39a2b6765bc62c7
moth dreams <mothdreams666@gmail.com>
2026-02-25 01:33:09 -0800

    sh3: match 2 `mall_b_00` functions --------- Co-authored-by: boredofspeedruns <lvl93roxas@gmail.com>

--- silent-hill-3/src/Event/mall_b_00.c
+++ silent-hill-3/src/Event/mall_b_00.c
@@ -1,10 +1,23 @@
-#include "common.h"
+#include "mall_b_00.h"
 
 INCLUDE_ASM("asm/nonmatchings/Event/mall_b_00", func_01F6D680_mall_b_00);
 
-INCLUDE_ASM("asm/nonmatchings/Event/mall_b_00", func_01F6D900_mall_b_00);
+void func_01F6D900_mall_b_00(float arg0)
+{
+    double t = func_00124120(arg0);
+    t = func_0012B398(t);
+    t = func_001248A0(1.0f, t * 1.667f);
+    func_00124CD8(t);
+}
 
-INCLUDE_ASM("asm/nonmatchings/Event/mall_b_00", func_01F6D960_mall_b_00);
+void func_01F6D960_mall_b_00(float arg0)
+{
+    func_00124CD8(
+        func_0012AB58(
+            func_00124120(1.0f / arg0)
+        )
+    );
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/mall_b_00", func_01F6D9A0_mall_b_00);
 
---
+++ silent-hill-3/src/Event/mall_b_00.h
@@ -0,0 +1,12 @@
+#ifndef MALL_B_00
+#define MALL_B_00
+
+#include "common.h"
+
+extern double func_0012AB58(double);
+extern double func_00124120(float);
+extern double func_0012B398(double);
+extern double func_001248A0(double, double);
+extern void func_00124CD8(double);
+
+#endif
