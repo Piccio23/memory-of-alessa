bd2068fa20f86e90a05005f47dc0ca5bf73fe45d
moth dreams <mothdreams666@gmail.com>
2026-02-28 10:23:48 -0800

    sh3: migrate remaining overlays to C

---
+++ silent-hill-3/src/Event/amusement_01.c
@@ -0,0 +1,79 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6D680_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6D7A0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6D7C0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6D840_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DC40_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DC70_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DCB0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DCF0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DDC0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DDE0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6DF10_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6E1A0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6E220_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6E2A0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6E710_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6E790_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6E810_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6E9A0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6F3C0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6F430_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6F4E0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6F5C0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6F6C0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6F7A0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6FA20_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6FB50_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6FC80_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6FDC0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F6FED0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F70000_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F701B0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F702A0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F70390_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F703B0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F70750_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F70780_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F70870_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F709D0_amusement_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/amusement_01", func_01F70A90_amusement_01);
---
+++ silent-hill-3/src/Event/hospital_f_01.c
@@ -0,0 +1,73 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6D680_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6D700_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6D7B0_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6D880_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6DA30_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6DAC0_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6DAD0_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6DAE0_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6DB50_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6DB70_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6DBA0_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6DCF0_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6DE00_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6E140_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6E1C0_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6E570_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6E600_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6E650_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6E850_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6EE60_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6EED0_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6EEF0_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6EFB0_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6F060_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6F380_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6F410_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6F640_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6F670_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6F690_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6F6C0_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6F7D0_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6F880_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6FBA0_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F6FD50_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F70000_hospital_f_01);
+
+INCLUDE_ASM("asm/nonmatchings/Event/hospital_f_01", func_01F70370_hospital_f_01);
