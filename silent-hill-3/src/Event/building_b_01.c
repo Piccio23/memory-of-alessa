bf2d2620025b3f70ec87eafd76365362d28eb3ef
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-02-28 18:31:34 +0100

    sh3: match func_01F6D970_building_b_01 & func_01F6E2A0_building_b_01 (#11)

--- silent-hill-3/src/Event/building_b_01.c
+++ silent-hill-3/src/Event/building_b_01.c
@@ -6,7 +6,15 @@ INCLUDE_ASM("asm/nonmatchings/Event/building_b_01", func_01F6D6E0_building_b_01)
 
 INCLUDE_ASM("asm/nonmatchings/Event/building_b_01", func_01F6D7E0_building_b_01);
 
-INCLUDE_ASM("asm/nonmatchings/Event/building_b_01", func_01F6D970_building_b_01);
+void func_01F6D970_building_b_01(void) {
+
+    if (func_0016C540(&D_01F6EFF0_building_b_01, &D_01F6F050_building_b_01) == 0) {
+        D_1D316AC |= 0x20000000; //cutscene start
+    } else {
+        D_1D316AC &= 0xDFFFFFFF; //cutscene end
+    }
+
+}
 
 INCLUDE_ASM("asm/nonmatchings/Event/building_b_01", func_01F6D9E0_building_b_01);
 
@@ -18,7 +26,25 @@ INCLUDE_ASM("asm/nonmatchings/Event/building_b_01", func_01F6DFF0_building_b_01)
 
 INCLUDE_ASM("asm/nonmatchings/Event/building_b_01", func_01F6E0F0_building_b_01);
 
-INCLUDE_ASM("asm/nonmatchings/Event/building_b_01", func_01F6E2A0_building_b_01);
+void func_01F6E2A0_building_b_01(void) {
+
+    switch (RoomName()) {                              
+        case BUILDING_OTHERWORLD_BATHTUB_ROOM:
+            if (((D_01D31640 >> 0x12) & 1) && !((D_01D31640 >> 0x13) & 1)) { //after the bathtub room awakening cutscene ???
+                func_001C2290(2, 0.0f); 
+            }
+            D_01F6F138_building_b_01 = 0;
+            D_01F6F130_building_b_01 = 0;
+            break;
+        case BUILDING_OTHERWORLD_HANGING_MOTHER_ROOM:
+            D_01F6F118_building_b_01 = 0;
+            D_1D317AC |= 0x20000; //first time entering hanging mother room
+            break;
+        case BUILDING_OTHERWORLD_VINCENT_CORRIDOR:
+            D_1D31714 |= 0x1000; //first time entering vincent corridor
+            break;
+    }
+}
 
 void func_01F6E360_building_b_01(void) {
 
@@ -44,8 +70,7 @@ void func_01F6E360_building_b_01(void) {
             func_00189FA0(0xF1, 0xF);
             D_1D31714 &= ~0x4000; //oxydol grab
             break;
-    
-        
+            
         case BUILDING_OTHERWORLD_VINCENT_CORRIDOR:
             if (!((D_1D3166C >> 0x1C) & 1)) { //check if player hasnt read heather's photo letter yet
                 func_0016CA40(6);
