d8d448e0b5f670ae507bb3ba4da7701b395936cb
moth dreams <mothdreams666@gmail.com>
2026-02-28 21:47:56 -0800

    sh3: split `hh_class_manager.c`

---
+++ silent-hill-3/src/Effect2/hh_class_manager.c
@@ -0,0 +1,89 @@
+#include "common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", ImpactManager);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141240);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_001413D0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_001413F0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141410);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141460);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_001414D0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141510);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141550);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_001415F0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141650);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", Instance_Search_from_InstanceHandle);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_001416B0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_001416F0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141730);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141820);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_001418B0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_001418D0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141910);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141980);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_001419D0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141A20);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141A90);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141AB0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141AE0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", Exception_Handling_Instance_Create);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", ClassAssociation_DataPool_Initialize);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141C80);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141D50);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", InstanceTable_All_Initialize);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141E50);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141EE0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141F70);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141F90);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00141FE0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", Object_Group_InstanceTableInfomeation_Set);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00142070);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_001420C0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_001420D0);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00142120);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00142130);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00142140);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", func_00142150);
+
+INCLUDE_ASM("asm/nonmatchings/Effect2/hh_class_manager", ObjectInstance_DataBlock_Get);
