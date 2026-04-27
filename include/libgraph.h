648528c16774e9a2e5a7dc2500c507e50113da11
moth dreams <mothdreams666@gmail.com>
2026-04-27 00:38:25 -0700

    sh2+3: update `libgraph.h`

--- include/libgraph.h
+++ include/libgraph.h
@@ -3,6 +3,7 @@
 
 #define SCE_GS_FALSE (0)
 #define SCE_GS_TRUE  (1)
+
 #define SCE_GS_PSMCT32			(0)
 #define SCE_GS_PSMCT24			(1)
 #define SCE_GS_PSMCT16			(2)
@@ -16,5 +17,22 @@
 #define SCE_GS_PSMZ24			(49)
 #define SCE_GS_PSMZ16			(50)
 #define SCE_GS_PSMZ16S			(58)
+
+#define SCE_GS_PRIM_POINT		(0)
+#define SCE_GS_PRIM_LINE		(1)
+#define SCE_GS_PRIM_LINESTRIP		(2)
+#define SCE_GS_PRIM_TRI			(3)
+#define SCE_GS_PRIM_TRISTRIP		(4)
+#define SCE_GS_PRIM_TRIFAN		(5)
+#define SCE_GS_PRIM_SPRITE		(6)
+#define SCE_GS_PRIM_IIP			(1<<3)
+#define SCE_GS_PRIM_TME			(1<<4)
+#define SCE_GS_PRIM_FGE			(1<<5)
+#define SCE_GS_PRIM_ABE			(1<<6)
+#define SCE_GS_PRIM_AA1			(1<<7)
+#define SCE_GS_PRIM_FST			(1<<8)
+#define SCE_GS_PRIM_CTXT1		(0)
+#define SCE_GS_PRIM_CTXT2		(1<<9)
+#define SCE_GS_PRIM_FIX			(1<<10)
 
 #endif
