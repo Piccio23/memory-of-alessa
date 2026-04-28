98599dd9e96891adcef1c7fb8e38e10e22bdac04
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-04-28 22:33:27 +0200

    update sh_sd_call.h

--- silent-hill-2/src/sound/sh_sd_call.h
+++ silent-hill-2/src/sound/sh_sd_call.h
@@ -9,5 +9,6 @@ int shSdCall(int i0 /* r19 */, int i1 /* r18 */, int i2 /* r17 */, int i3 /* r16
 void shSd3dAllStop(void);
 void shSd3dStop(int i0 /* r16 */);
 int shSdSeStop(int i0 /* r16 */);
+void shSdVSync(void);
 
 #endif // SH_SD_CALL_H
