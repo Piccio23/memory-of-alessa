11990c927c6fe87282e3dc5fa804f9aae0d34b81
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-07-13 17:56:08 +0200

    Update m3_eddie.h

--- silent-hill-2/src/Chacter/m3_eddie.h
+++ silent-hill-2/src/Chacter/m3_eddie.h
@@ -4,6 +4,13 @@
 #include "sh2_common.h"
 #include "Chacter/character.h"
 
+#define EDDIE_DRAMA_ANIME_ID_START 4500
+#define EDDIE_PLAY_ANIME_ID_START  4800
+
+#define EDDIE_PLAY_ANIM_4801       4801
+#define EDDIE_PLAY_ANIM_4802       4802
+#define EDDIE_PLAY_ANIM_4803       4803
+
 void shCharacterSetHumanEDILow(SubCharacter* scp);
 int shCharacterHumanEDIAnimeSet(SubCharacter* scp, int anime_id);
 int shCharacterHumanEDIAnimeSetP(SubCharacter* scp, int anime_id);
