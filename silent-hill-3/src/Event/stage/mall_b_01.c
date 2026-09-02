cf004371c13eb596a2cd8968e4c26361e34bd5c5
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-09-02 10:32:08 +0200

    match 2 mall_b_01.c

--- silent-hill-3/src/Event/stage/mall_b_01.c
+++ silent-hill-3/src/Event/stage/mall_b_01.c
@@ -1,6 +1,13 @@
-#include "mall_b_01.h"
+#include "Event/stage/mall_b_01.h"
 
+#include "vec.h"
+
+#include "Collision/cl_main.h"
+
+#include "Chacter/chara_list.h"
 #include "Chacter/m3_sc.h"
+
+#include "Font/font.h"
 
 INCLUDE_ASM("asm/nonmatchings/Event/stage/mall_b_01", func_01F6D680_mall_b_01);
 
@@ -78,9 +85,89 @@ int func_01F6D860_mall_b_01(void) {
 
 INCLUDE_ASM("asm/nonmatchings/Event/stage/mall_b_01", func_01F6DA50_mall_b_01);
 
-INCLUDE_ASM("asm/nonmatchings/Event/stage/mall_b_01", func_01F6DC10_mall_b_01);
+int func_01F6DC10_mall_b_01(void) {
+    sceVu0FVECTOR vec1;
+    sceVu0FVECTOR vec2; 
+    SubCharacter* scp;
+    
+    if (!GET_BIT(D_1D31658, 0x14)) {
+        D_01F728C8_mall_b_01 = 0;
+        if ((GET_BIT(D_1D31658, 0x15))) {
+            D_01F728C0_mall_b_01 = 27;
+        } else {
+            D_01F728C0_mall_b_01 = 26;
+        }
+        SET_BIT(D_1D31658, 0x14);
+    }
+    
+    if (func_00190240() != 0) {
+        fontClear();
+        D_01F728C8_mall_b_01 = 3;
+    }
+    
+    if (D_01F728C8_mall_b_01 != 0) {
+        clAddDynamicWall(&D_01F719C0_mall_b_01);
+    }
+    
+    switch (D_01F728C8_mall_b_01) {                  
+        case 0:
+            scp = (SubCharacter*)shCharacterGetPlayer();
+            scp->pos.x += -50.0f;
+            volatile_vec_copy(&vec1, &scp->pos);
+            volatile_vec_copy(&vec2, &scp->pos);
+            vec1[0] += 1000.0f;
+            vec2[0] -= 1000.0f;
+            func_001E0130(NULL, vec1, vec2, 217);
+            D_01F728C8_mall_b_01++;
+            break;
+    
+        case 1:
+            scp = (SubCharacter*)shCharacterGetPlayer();
+            if (scp->pos.x > 52900.0f) {
+                scp->pos.x += -50.0f;
+            }
+    
+            if (func_00190D90() == 0) {
+                break; 
+            }
+            
+            func_00168430();
+            func_00190A20(2);
+            D_01F728C8_mall_b_01++;
+        
+        case 2:
+            if (func_0016C1C0(D_01F728C0_mall_b_01) == 0) {
+                break; 
+            }
+            
+            D_01F728C8_mall_b_01++;
+    
+        default:
+            if (!GET_BIT(D_1D31658, 0x15)) {
+                SET_BIT(D_1D31658, 0x15);
+            }
+            UNSET_BIT(D_1D31658, 0x14);
+            func_00190A20(0);
+            return 1; 
+    }
+    
+    return 0;
+}
 
-INCLUDE_ASM("asm/nonmatchings/Event/stage/mall_b_01", func_01F6DE60_mall_b_01);
+int func_01F6DE60_mall_b_01(void) {
+    SubCharacter* scp;
+    sceVu0FVECTOR vec1;
+    sceVu0FVECTOR vec2; 
+    
+    scp = (SubCharacter*)shCharacterGetPlayer();
+    scp->pos.x += -50.0f; 
+    volatile_vec_copy(&vec1, &scp->pos);
+    volatile_vec_copy(&vec2, &scp->pos);
+    vec1[0] += 500.0f;
+    vec2[0] -= 500.0f;
+    func_001E0130(NULL, vec1, vec2, 218);
+    return 1;
+}
 
 int func_01F6DEE0_mall_b_01(void) {
     func_0012CFA0();
