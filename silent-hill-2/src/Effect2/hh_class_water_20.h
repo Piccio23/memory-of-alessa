b8050f909b6d3912c2d832b61a719d06fb64a097
Piccio23 <105627859+Piccio23@users.noreply.github.com>
2026-09-20 22:02:39 +0200

    update `hh_class_water_20.h`

--- silent-hill-2/src/Effect2/hh_class_water_20.h
+++ silent-hill-2/src/Effect2/hh_class_water_20.h
@@ -1,4 +1,39 @@
 #ifndef HH_CLASS_WATER_20_H
 #define HH_CLASS_WATER_20_H
 
+#include "sh2_common.h"
+#include "Effect2/hh_class_manager.h"
+#include "Effect2/hh_class_water_common.h"
+#include "sce/libvu0.h"
+
+typedef struct Motion_Table_Infomeation {
+    float diff_time;        // offset 0x0, size 0x4
+    float diff_scale_ratio; // offset 0x4, size 0x4
+    float diff_rgba_ratio;  // offset 0x8, size 0x4
+} Motion_Table_Infomeation;
+
+typedef struct Vertex_Infomeation_List {
+    float (*pVertex_List)[4]; // offset 0x0, size 0x4
+    float (*pNormal_List)[4]; // offset 0x4, size 0x4
+    u_int Vertex_Max;         // offset 0x8, size 0x4
+    u_int Primitive_Type;     // offset 0xC, size 0x4
+} Vertex_Infomeation_List;
+
+// total size: 0x40
+typedef struct HH_Object_Water_20 {
+    // Members
+    struct Object_DataBlock_Header Header; // offset 0x0, size 0x10
+    unsigned int Step; // offset 0x10, size 0x4
+    unsigned int Vertex_Kind; // offset 0x14, size 0x4
+    float Timer; // offset 0x18, size 0x4
+    sceVu0FVECTOR Verocity_0; // offset 0x20, size 0x10
+    float Scale; // offset 0x30, size 0x4
+    float Alpha; // offset 0x34, size 0x4
+    unsigned int Motion_Step; // offset 0x38, size 0x4
+} HH_Object_Water_20;
+
+u_int HH_Class_Prefix_Water_20(void);
+u_int HH_Class_Suffix_Water_20(void);
+u_int HH_Class_Water_20(void* pBlock, ImpactQueue_Element* pElement);
+
 #endif // HH_CLASS_WATER_20_H
